b776796d8b8d49641a54ea785cd6887a917e94fc
Heena <meheenakaushar@gmail.com>
2021-03-01 12:47:46 +0530

    Initial commit

---
+++ src/components/Home.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from "react";
+import { useLocation, useHistory } from "react-router-dom";
+import apiUtils from "../utils/apiUtils";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+
+import "./Home.css";
+import Footer from "./Footer";
+import LaunchesList from "./launchesList";
+
+export default function Home() {
+  const [launches, setLaunches] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const location = useLocation();
+
+  const history = useHistory();
+
+  useEffect(() => {
+    const fetchLaunches = async () => {
+      const res = await apiUtils.fetchLaunches(location.search);
+      setLaunches(res);
+      setLoading(false);
+    };
+
+    fetchLaunches();
+  }, [location.search]);
+
+  const handleUpdateSearchString = (searchString) => {
+    setLoading(true);
+    history.push(searchString);
+  };
+
+  return (
+    <div className="custom-container home">
+      <div className="custom-row max-width-xl">
+        <Header />
+      </div>
+      <div className="custom-row max-width-xl">
+        <div className="sidebar-col">
+          <Sidebar updateSearchString={handleUpdateSearchString} />
+        </div>
+        <div className="main-area-col ">
+          <LaunchesList loading={loading} launches={launches} />
+        </div>
+      </div>
+      <div className="custom-row max-width-xl">
+        <Footer />
+      </div>
+    </div>
+  );
+}
